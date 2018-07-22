24ccf4aeab71889539d0c65dcd6ddb162229cf0c
ryu2020 <ryu2020@pingry.org>
2018-07-22 02:43:55 -0400

    i still don't get bootstrap

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+app.get('/music', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/music.html'));
+});
+
+app.get('/programming', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/programming.html'));
+});
+
 app.get('*', function(req, res){
   res.sendFile(path.join(__dirname + '/views/404.html'));
 });
