4c77879289f03230111cebc32b3d6e32b9bce522
ryu2020 <ryu2020@pingry.org>
2018-07-22 02:02:51 -0400

    i dont get bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+//import 'bootstrap';
+
 var express = require('express')
 var app = express()
 var path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname + '/views/404.html'));
 });
 
 app.listen(3000, function(){
